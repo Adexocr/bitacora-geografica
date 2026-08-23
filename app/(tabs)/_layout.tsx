1372c40f7c2a24f9f9f53b688f82f3bcc107affc
Kadem <kxtarx000@gmail.com>
2026-08-23 11:24:51 -0600

    Creacion de los dos tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,22 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: '#6C5CE7' }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Capturar',
+          tabBarIcon: ({ color, size }) => <Ionicons name="camera" color={color} size={size} />,
         }}
-      />
+        />
       <Tabs.Screen
-        name="explore"
+        name="galeria"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Galería',
+          tabBarIcon: ({ color, size }) => <Ionicons name="images" color={color} size={size} />,
         }}
       />
     </Tabs>
